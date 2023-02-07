77709bd813ec3c95f725a4c47c484b301c1795cf
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-02-07 08:33:26 -0700

    Add project files.

---
+++ ITEC145-Section-E/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITEC145_Section_E
+{
+    class Account
+    {
+        //Variables to include in one account.
+
+        private string _accNum;
+        private string _firstName;
+        private string _lastName;
+        private string _balance;
+
+        public string AccNum {
+            get { return _accNum; }
+            set { _accNum = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+    }
+}
---
+++ ITEC145-Section-E/Form1.cs
@@ -0,0 +1,10 @@
+namespace ITEC145_Section_E
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
